6bdf916899a8d2d696d4ae292260a24c778f8bb2
ajhurst95 <ajhurst95@gmail.com>
2017-07-19 16:35:29 -0400

    updating site

---
+++ babylonterminal/www/html/mobileredirect.js
@@ -0,0 +1,7 @@
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        window.location.href = "https://www.google.com";
+    } else {
+
+
+
+    	//code to redirect to another site if mobile site. 
