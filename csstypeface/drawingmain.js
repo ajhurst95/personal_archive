5b564bfb29b8523410b57f16b79dc498a3a641ca
ajhurst95 <ajhurst95@gmail.com>
2017-08-02 13:39:17 -0400

    last commit

--- csstypeface/drawingmain.js
+++ csstypeface/drawingmain.js
@@ -1,20 +1,24 @@
-// $(document).ready(function(){
-// 	$("#play").toggle(
-// 		function(){
-// 			document.getElementById('#forecast').play();
-// 		},
-// 		function(){
-// 			document.getElementById('#forecast').pause();
-// 		});
-// });
-$(document).ready(function(){
-	$('#play').toggle(
-	function () {
-	document.getElementById('forecast').play();
-	},
-	function () {
-	document.getElementById('forecast').pause();
-	}
-	);
+$(document).ready(function() {
+    var playing = false;
+
+    $('#play').click(function() {
+
+        $(this).toggleClass("pause");
+
+        if (playing == false) {
+            document.getElementById('forecast').play();
+            playing = true;
+
+        } else {
+            document.getElementById('forecast').pause();
+            playing = false;
+        }
+    });
+
+    $('#hide').click(function() {
+        $(this).toggleClass("hiddenbutton");
+        $('li.letter').toggleClass("hidevid");
+        $('.video').toggleClass("hidden");
+        $('.letter.zero.slash').toggleClass("hidden");
+    });
 });
-	
